830ef57377c9425a9f4b4a8b25f69e013f604312
iwillwen <willwengunn@gmail.com>
2019-03-21 17:10:23 +0800

    update

--- src/main.js
+++ src/main.js
@@ -1,8 +1,16 @@
 import Vue from 'vue'
-import App from './App.vue'
+import Vuex from 'vuex'
+import iView from 'iview'
+import 'iview/dist/styles/iview.css'
 
+Vue.use(Vuex)
+Vue.use(iView, {
+  size: 'large'
+})
 Vue.config.productionTip = false
 
+const App = require('./App.vue')
+
 new Vue({
-  render: h => h(App),
+  render: h => h(App.default),
 }).$mount('#app')
